a4d785004f9dc8a85a12ba50dcc1ca2c10bba8cd
cf904c27 <34496078+cf904c27@users.noreply.github.com>
2019-01-08 11:51:23 +0800

    Update util.py

--- util.py
+++ util.py
@@ -79,8 +79,8 @@ def evaluate(model, dataset, args, sess):
             NDCG += 1 / np.log2(rank + 2)
             HT += 1
         if valid_user % 100 == 0:
-            print '.',
-            sys.stdout.flush()
+            print ('.',
+            sys.stdout.flush())
 
     return NDCG / valid_user, HT / valid_user
 
